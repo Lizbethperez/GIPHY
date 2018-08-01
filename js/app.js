5f632528fddcd5e49a458552e81306e42652084d
Lizbethperez <lizbeth_perez_v@hotmail.com>
2018-08-01 13:56:00 -0500

    Se suben avances en ejercicio giphy

---
+++ js/app.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+   var dibujarGifs=function(data){
+    var gif ="";
+    var url = "";
+    data.forEach(function(element) {
+        gif=element.images.downsized_large.url;
+        url=element.bitly_gif_url;
+        $("#elementos").append(armarTemplate(gif,url));
+    });
+  }
+
+var armarTemplate = function(gif,url){
+    var t = "<div class='elemento'><img src='"+gif +"'/><a href='"+url+"'>ver mas</a></div"
+    return t;
+}
+
+var ajaxGif= function(gif){
+    $.ajax({
+        url: 'https://api.giphy.com/v1/gifs/search',
+        type:'GET',
+        datatype:'json',
+        data:{
+            q:gif,
+            api_key:'RaC4BnYtuyTMdT6YPOQe3KJCP4qoQtsI'
+        }
+    })
+    .done(function(response){
+        console.log(response);
+        dibujarGifs(response.data);
+    })
+    .fail(function(){
+        console.log("error");
+    })
+}
+
+$("#buscar-gif").click(function(event){
+    console.log("entro");
+    $("#elementos").empty();
+    var gif=$("#gif-text").val();
+    ajaxGif(gif);
+    });
+});
